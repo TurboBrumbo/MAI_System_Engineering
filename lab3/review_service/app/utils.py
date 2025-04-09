058604bb67e333ad1a2aba6f7266097687106318
Maxim Kras <134211079+TurboBrumbo@users.noreply.github.com>
2025-04-10 02:56:59 +0300

    Delete lab3 directory

--- lab3/review_service/app/utils.py
+++
@@ -1,4 +0,0 @@
-def validate_review_data(review_data: dict):
-    """Пример функции валидации"""
-    required_fields = ['title', 'author']
-    return all(field in review_data for field in required_fields)
